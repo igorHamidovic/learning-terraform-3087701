d54f96943478e3f757608b9ae690a14cb525f6e4
igorHamidovic <igorweg@outlook.com>
2025-09-17 15:36:27 +0200

    rollback

--- main.tf
+++ main.tf
@@ -33,19 +33,17 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-  
-  image_id           = data.aws_ami.app_ami.id
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 module "blog_alb" {
