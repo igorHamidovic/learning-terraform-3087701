a485dc6e624d36881981615a00818043e025a517
igorHamidovic <igorweg@outlook.com>
2025-09-17 14:21:32 +0200

    fix

--- main.tf
+++ main.tf
@@ -59,6 +59,9 @@ module "alb" {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
+      forward = {
+        target_group_key = "ex-instance"
+      }
     }
   }
 
